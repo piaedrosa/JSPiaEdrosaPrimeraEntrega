4bc6eab2ce1559196ac80e33b92fa580d880da19
Pia Edrosa <piaedrosa@gmail.com>
2023-08-31 14:55:11 -0300

    agregar proyecto disenioweb

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+console.log("-->Hello World")
+let mensaje = "-->Hello World-->"
+console.log (mensaje)
+
+let numeroUno= 13
+let numeroDos= 21
+let resultado = numeroUno + numeroDos
+console.log(resultado);
