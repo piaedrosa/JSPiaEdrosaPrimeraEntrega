7741f50d02255d5bd128ef93fce71dad7102f093
Pia Edrosa <piaedrosa@gmail.com>
2023-09-13 15:04:43 -0300

    convertir funciones en f flecha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // Lo primero que hacemos es una función para validar la edad del usuario
-function validarEdad() {
+const validarEdad=()=> {
     let edad = parseInt(prompt("Por favor, ingresa tu edad:"));
   
     if (isNaN(edad) || edad <= 0) {
@@ -16,7 +16,7 @@ function validarEdad() {
   }
   
   // cuando valida que es mayor, pasa a la función para elegir un plan (en funcion a la edad)
-  function elegirPlan() {
+  const elegirPlan = ()=> {
     let plan = prompt("Elige un plan: scaled, intermedio, elite").toLowerCase();
   
     if (plan === "scaled" || plan === "intermedio" || plan === "elite") {
@@ -27,29 +27,30 @@ function validarEdad() {
   }
   
   // El costo mensual varia segun el metodo de pago. creo otra función para elegir el método de pago, que depende del plan
-function elegirMetodoDePago(plan) {
-    let metodoPago = prompt("Elige un método de pago: efectivo, crédito, débito").toLowerCase();
+const elegirMetodoDePago=(plan)=> {
+    let metodoPago = prompt("Elige un método de pago: efectivo, credito, debito").toLowerCase();
     let costoMensual = calcularCostoMensual(plan);
   
     switch (metodoPago) {
       case "efectivo":
         costoMensual *= 0.9; // Aplicar un descuento del 10% al pagar en efectivo
-        console.log("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago en efectivo).");
+        alert("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago en efectivo).");
         break;
-      case "crédito":
+      case "credito":
         costoMensual *= 1.1; // Aplicar un recargo del 10% al pagar con crédito
-        console.log("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago con tarjeta de crédito).");
+        alert("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago con tarjeta de crédito).");
         break;
-      case "débito":
+      case "debito":
         costoMensual *= 1.05; // Aplicar un recargo del 5% al pagar con débito
-        console.log("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago con tarjeta de débito).");
+        alert("El costo mensual del plan " + plan + " es: $" + costoMensual + " (Pago con tarjeta de débito).");
         break;
       default:
-        console.log("Método de pago no válido. Por favor, elige uno de los métodos de pago disponibles.");
+        alert("Método de pago no válido. Por favor, elige uno de los métodos de pago disponibles.");
+
     }
   }
   // Usamos un Switch dentro de la función para calcular el costo mensual según el plan
-  function calcularCostoMensual(plan) {
+  const calcularCostoMensual=(plan)=> {
     switch (plan) {
       case "scaled":
         return 10000;
@@ -64,3 +65,75 @@ function elegirMetodoDePago(plan) {
   
   // iniciamos el proceso instanciando a la función principal 
   validarEdad();
+
+
+  //*13/9 => Ejercicio de clase.
+  /* Objeto que controla el funcionamiento del programa
+const PlanDeSuscripcion=()=> {
+    this.edad = 0;
+    this.plan = "";
+    this.metodoPago = "";
+  
+    // Constructor para solicitar la edad del usuario
+    this.validarEdad = function () {
+      this.edad = parseInt(prompt("Por favor, ingresa tu edad:"));
+  
+      if (isNaN(this.edad) || this.edad <= 0) {
+        alert("Ingresa una edad válida.");
+        return;
+      }
+  
+      if (this.edad < 16) {
+        alert("Eres menor de 16 años y no puedes suscribirte a nuestros planes.");
+        return;
+      }
+  
+      this.elegirPlan();
+    };
+  
+    // Método para elegir un plan
+    this.elegirPlan = function () {
+      this.plan = prompt("Elige un plan: scaled, intermedio, elite").toLowerCase();
+  
+      if (this.plan === "scaled" || this.plan === "intermedio" || this.plan === "elite") {
+        this.elegirMetodoDePago();
+      } else {
+        alert("Plan no válido. Por favor, elige uno de los planes disponibles.");
+      }
+    };
+  
+    // Método para elegir el método de pago
+    this.elegirMetodoDePago = function () {
+      this.metodoPago = prompt("Elige un método de pago: efectivo, credito, debito").toLowerCase();
+      let costoMensual = this.calcularCostoMensual();
+  
+      if (this.metodoPago === "efectivo") {
+        alert("El costo mensual del plan " + this.plan + " para una persona de " + this.edad + " años es: $" + costoMensual + " (Pago en efectivo).");
+      } else if (this.metodoPago === "credito") {
+        alert("El costo mensual del plan " + this.plan + " para una persona de " + this.edad + " años es: $" + costoMensual + " (Pago con tarjeta de crédito).");
+      } else if (this.metodoPago === "debito") {
+        alert("El costo mensual del plan " + this.plan + " para una persona de " + this.edad + " años es: $" + costoMensual + " (Pago con tarjeta de débito).");
+      } else {
+        alert("Método de pago no válido. Por favor, elige uno de los métodos de pago disponibles.");
+      }
+    };
+  
+    // Método para calcular el costo mensual según el plan
+    this.calcularCostoMensual = function () {
+      switch (this.plan) {
+        case "scaled":
+          return 10000;
+        case "intermedio":
+          return 15000;
+        case "elite":
+          return 20000;
+        default:
+          return 0;
+      }
+    };
+  }
+  
+  // Crear una instancia del objeto y utilizarla para iniciar el programa
+  let planSuscripcion = new PlanDeSuscripcion();
+  planSuscripcion.validarEdad(); */
+  
